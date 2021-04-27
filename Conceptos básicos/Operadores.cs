3c27cb6a5b7268170e99de41687b3ef83ccd5345
Daniel Rosas <dirm2705@gmail.com>
2021-04-27 11:40:06 -0500

    Conceptos básicos

--- Conceptos básicos/Operadores.cs
+++ Conceptos básicos/Operadores.cs
@@ -45,5 +45,38 @@ namespace Conceptos_básicos
             x = (25 + 9) * 2;
             //Resultado 68
         }
+
+        void OperadoresDeAsignación()
+        {
+            float x = 5; //El signo igual en esta linea es un operador de asignación. Asigna el 5 a la variable x
+
+            /*Los operadores de asignación compuestos modifican el valor de una variable tomando en cuenta su valor inicial
+            Se hacen a partir del signo igual y un operador aritmético*/
+
+            //Ejemplos
+
+            x += 2; //Eso es lo mismo que x = x + 2
+
+            x *= 5;  //Eso es lo mismo que x = x * 5
+
+            x /= 7; //Eso es lo mismo que x = x / 7
+        }
+
+        void OperadoresDeIncrementoYDecremento()
+        {
+            float x = 0;
+            //Este operador incrementa o decrementa en uno el valor de la variable
+            //Hay dos formas de usarlo. Cómo prefijo o sufijo.
+
+            //En la forma prefijo primero se cambia el valor de la variable y después se toma el valor
+
+            //Ejemplo
+            float y = ++x; //y = 1, x = 1
+
+
+            //En la forma sufijo primero se toma el valor de la variable y después se cambia el valor
+
+            float z = x--; //z = 0, x = -1
+        }
     }
 }
